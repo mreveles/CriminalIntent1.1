b968e7381c99c907e6f7316d3267571cc32f3e6d
mareveles <mareveles1@buffs.wtamu.edu>
2018-02-24 18:46:20 -0600

    deleted empty spaces added cribelab to branch

---
+++ app/src/main/java/com/spring2018/cidm4385/mareveles/criminalintent/CrimeLab.java
@@ -0,0 +1,30 @@
+package com.spring2018.cidm4385.mareveles.criminalintent;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+       /* mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
+        */
+    }
+}
